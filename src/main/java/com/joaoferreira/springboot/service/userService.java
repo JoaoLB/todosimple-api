decd218a13a1722410ae19325fa7bc8528e8dc12
João Ferreira <vitornetworks@outlook.com>
2023-11-13 12:49:00 -0300

    User Service

---
+++ src/main/java/com/joaoferreira/springboot/service/userService.java
@@ -0,0 +1,52 @@
+package com.joaoferreira.springboot.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.joaoferreira.springboot.model.userModel;
+import com.joaoferreira.springboot.repository.taskRepository;
+import com.joaoferreira.springboot.repository.userRepository;
+
+@Service
+public class userService {
+    
+    @Autowired
+    private userRepository userRepository;
+    @Autowired
+    private taskRepository taskRepository;
+
+    public userModel findById(Long id){
+        Optional<userModel> userModel = this.userRepository.findById(id);
+        return userModel.orElseThrow(() -> new RuntimeException(
+            "Usuário não encontrado!"
+        ));
+    }
+
+    @Transactional
+    public userModel create(userModel obj){
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public userModel update(userModel obj){
+        userModel newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try{
+            this.userRepository.deleteById(id);
+        } catch (Exception e){
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
+        }
+    }
+
+}
